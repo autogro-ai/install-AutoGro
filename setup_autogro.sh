af3a04f49287b13e2a6af8ef3a9daa68c1666cfe
AutoGro <131834659+autogro-ai@users.noreply.github.com>
2023-06-17 11:12:42 -0500

    Update setup_autogro.sh

--- setup_autogro.sh
+++ setup_autogro.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-cd "$HOME"
 failed_tasks=""
+
+if pip3 install adafruit-circuitpython-mcp3xxx; then
+    echo "Installed adafruit-circuitpython-mcp3xxx library."
+else
+    echo "Failed to install adafruit-circuitpython-mcp3xxx library."
+    failed_tasks+="Library installation "
+fi
+
+cd "$HOME"
 
 if [ ! -d "bin" ]; then
     mkdir bin
@@ -11,45 +19,47 @@ else
 fi
 
 cd "$HOME/bin/"
-if git clone https://github.com/autogro-ai/AutoGro.git; then
-    echo "AutoGro repository cloned successfully."
-    cd "$HOME/bin/AutoGro/"
-    sudo chmod 755 AutoGro/
-    if sudo chmod 755 AutoGro/; then
-        "Permissions set successfully for 'AutoGro' directory."
-    else
-    echo "Failed to set permissions for 'AutoGro' directory."
-    failed_tasks="Permission setup "
-    fi
-    cd "$HOME/bin/AutoGro/Scripts"
-    
-    if crontab crontab.pi; then
-        if crontab -l | grep -q "Setting up logs to record at midnight, see more at /Scripts/crontab.pi"; then
-            echo "Crontab file added to the cron scheduler."
-        else
-            echo "Failed to add crontab file. Verification failed."
-            failed_tasks+="Crontab setup "
-        fi
-    else
-        echo "Failed to add crontab file. Command execution failed."
-        failed_tasks+="Crontab setup "
-    fi
 
-    if pip3 install adafruit-circuitpython-mcp3xxx; then
-        echo "Installed adafruit-circuitpython-mcp3xxx library."
-        echo "Successfully downloaded AutoGro and set up environment."
-        echo "Confused on what to do next? Read the docs at https://autogro.gitbook.io/autogro-docs/."
-        echo "Found a bug? Let us know: https://github.com/autogro-ai/AutoGro/issues"
-        echo "Join our community at: https://discord.gg/mWMFBDhg!"
-        echo "Thank you for your support. We couldn't do this without our community."
-    else
-        echo "Failed to install adafruit-circuitpython-mcp3xxx library."
-        failed_tasks+="Library installation "
-    fi
+repository="https://github.com/autogro-ai/AutoGro.git"
+
+target_dir="$HOME/bin/AutoGro"
+
+mkdir -p "$HOME/bin"
+
+cd "$HOME/bin" || exit
+
+if git clone "$repository"; then
+    echo "AutoGro repository cloned successfully."
+    cd "$target_dir" || exit
 else
     echo "Failed to clone AutoGro repository."
     failed_tasks+="Repository cloning "
 fi
 
-echo "Path of the current file: $(readlink -f $0)"
-echo "Tasks that failed: $failed_tasks"
+cd "$HOME/bin/AutoGro/Scripts"
+
+sudo cp rc.local /etc/
+sudo crontab crontab.pi
+if crontab crontab.pi; then
+    if crontab -l | grep -q "Setting up logs to record at midnight, see more at /Scripts/crontab.pi"; then
+        echo "Crontab file added to the cron scheduler."
+    else
+        echo "Failed to add crontab file. Verification failed."
+        failed_tasks+="Crontab setup "
+    fi
+else
+    echo "Failed to add crontab file. Command execution failed."
+    failed_tasks+="Crontab setup "
+fi
+
+
+echo "\n=========================================================================="
+echo "Successfully downloaded AutoGro and set up the environment.\n\n"
+echo "Check out our docs at https://autogro.gitbook.io/autogro-docs/."
+echo "Found a bug? Let us know: https://github.com/autogro-ai/AutoGro/issues"
+echo "\n==Join our Discord Community==\n\nhttps://discord.gg/mWMFBDhg!\n"
+echo "Thank you for your support. We couldn't do this without our community."
+echo "==========================================================================\n"
+
+
+echo "Tasks that failed to install: $failed_tasks"
